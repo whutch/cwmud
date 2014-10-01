95d1264d66dd5c24fd672866a54ef17774f3a0b1
Will Hutcheson <will.hutcheson@gmail.com>
2014-10-01 14:58:32 -0500

    Basic Project Structure
    Add project init file along with a skeleton directory structure.
    Add settings file.

---
+++ atria/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""A modular MUD server"""
+# Part of Atria MUD Server (https://github.com/whutch/atria)
+# :copyright: (c) 2008 - 2014 Will Hutcheson
+# :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
+
+from os.path import abspath, dirname
+
+__author__ = "Will Hutcheson"
+__contact__ = "will@whutch.com"
+__homepage__ = "https://github.com/whutch/atria"
+__license__ = "MIT"
+__docformat__ = "restructuredtext"
+
+VERSION = (0, 1, 0, "dev", "Applesauce")
+
+
+def get_version():
+    """Return the version string."""
+    return "{}{}".format(".".join([str(n) for n in VERSION[:3]]),
+                         "-{}".format(VERSION[3]) if VERSION[3] else "")
+
+
+def get_codename():
+    """Return the codename for this version."""
+    return VERSION[4]
+
+
+__version__ = "{} ({})".format(get_version(), get_codename())
+
+ROOT_DIR = dirname(dirname(abspath(__file__)))
