bf643f7c8e3e79c1273aaafd4b88e878ef39e5eb
Will Hutcheson <will.hutcheson@gmail.com>
2014-10-01 15:04:02 -0500

    Py.test Conftest File
    Add a conftest.py file for setting up testing through py.test.

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Configuration for testing through py.test"""
+# Part of Atria MUD Server (https://github.com/whutch/atria)
+# :copyright: (c) 2008 - 2014 Will Hutcheson
+# :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
+
+from os.path import abspath, dirname, join
+import sys
+
+# Until Atria is an installable package, we need to add it to our Python path
+#  so that we can do "from atria" imports in the tests.
+TEST_ROOT = dirname(abspath(__file__))
+sys.path.insert(0, dirname(TEST_ROOT))
+
+
+from atria import ROOT_DIR, settings
+
+# Change the log path during testing
+settings.LOG_PATH = join(ROOT_DIR, "logs", "test.log")
