036de9d974da9d027a5c45850b355d3698fa48bd
Will Hutcheson <will.hutcheson@gmail.com>
2014-10-02 13:23:19 -0500

    Additional Project Structure
    Add opt and utils packages to the atria\core package. Add headers with
    package descriptions to all empty init files.

--- atria/core/utils/__init__.py
+++ atria/core/utils/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Third party libraries packaged with changes"""
+"""Utility support server modules"""
 # Part of Atria MUD Server (https://github.com/whutch/atria)
 # :copyright: (c) 2008 - 2014 Will Hutcheson
 # :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
