d5ec27bc6b2548bbdf3b27f87bf80ad034fa4015
Will Hutcheson <will.hutcheson@gmail.com>
2014-10-08 22:45:49 -0500

    Sessions Module
    Add sessions.py module to the atria/core package, along with tests.

---
+++ atria/core/sessions.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""Session management and client IO."""
+# Part of Atria MUD Server (https://github.com/whutch/atria)
+# :copyright: (c) 2008 - 2014 Will Hutcheson
+# :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
+
+from collections import deque
+
+from .. import settings
+from .events import EVENTS
+from .logs import get_logger
+from .utils.exceptions import AlreadyExists
+from .utils.funcs import joins
+from .utils.mixins import HasFlags
+
+
+log = get_logger("sessions")
+
+
+class SessionManager:
+
+    """A manager for client sessions."""
+
+    def __init__(self):
+        self.greeting = "Welcome!\n"
+        self._sessions = []
+
+    def find_by_socket(self, socket):
+        """Find a session by its socket.
+
+        :param miniboa.TelnetClient socket: The socket to search for
+        :returns _Session|None: A matching session or None
+
+        """
+        for session in self._sessions:
+            # noinspection PyProtectedMember
+            if session._socket is socket:
+                return session
+
+    def create(self, socket):
+        """Create a new session tied to the given socket.
+
+        :param miniboa.TelnetClient socket: The socket to tie to the session
+        :returns _Session: The new session
+        :raises AlreadyExists: If a session with that socket already exists
+
+        """
+        if self.find_by_socket(socket):
+            raise AlreadyExists(socket, self.find_by_socket(socket))
+        session = _Session(socket)
+        self._sessions.append(session)
+        return session
+
+    def poll(self):
+        """Poll all sessions for queued IO."""
+        for session in self._sessions:
+            session.poll()
+
+    def prune(self):
+        """Clean up closed or dead sessions."""
+        self._sessions = [session for session in self._sessions
+                          if session.active]
+
+
+class _Session(HasFlags):
+
+    """A client session, sending and receiving data through a socket."""
+
+    def __init__(self, socket):
+        """Create a new session tied to a socket.
+
+        Don't do this yourself, call SessionManager.create instead.
+
+        """
+        super().__init__()
+        self._address = socket.addrport().rsplit(":", 1)[0]
+        self._output_queue = deque()
+        self._request_queue = deque()
+        self._socket = socket
+
+    def __del__(self):
+        self._socket.sock.close()
+        self._socket.active = False
+
+    def __repr__(self):
+        return joins("Session<", self._address, ">", sep="")
+
+    @property
+    def active(self):
+        """Return whether this session is still active."""
+        return (not self.flags.has_any("close", "closed", "dead")
+                and self._socket and self._socket.active)
+
+    @property
+    def address(self):
+        """Return the address this session is connected from."""
+        return self._address
+
+    def _check_idle(self):
+        """Check if this session is idle."""
+        idle = self._socket.idle()
+        if idle >= settings.IDLE_TIME:
+            if idle >= settings.IDLE_TIME_MAX:
+                # They've been idle long enough, dump them.
+                self.close("^RDisconnecting due to inactivity. Goodbye!^~",
+                           log_msg=joins("Disconnecting idle", self))
+            elif "idle" not in self.flags:
+                with EVENTS.fire("session_idle"):
+                    log.info("%s is now idle", self)
+                    self.flags.add("idle")
+        elif "idle" in self.flags:
+            # They came back, huzzah.
+            with EVENTS.fire("session_idle_return"):
+                log.info("%s is no longer idle", self)
+                self.flags.drop("idle")
+
+    def _parse_input(self, data):
+        """Process input from the client."""
+        # Placeholder parsing, for now.
+        if data.strip() == "quit":
+            self.close("Okay, goodbye!", log_msg=joins(self, "has quit"))
+        else:
+            self.send("You sent:", data)
+
+    def _get_prompt(self):
+        """Generate the current prompt for this session."""
+        prompt = "^y>^~ "
+        return prompt
+
+    def send(self, data, *more, sep=" ", end="\n"):
+        """Send text to the client tied to this session.
+
+        The resulting output will not be sent immediately, but will be put
+        in a queue to be sent during its next poll.
+
+        `message` and all members of `more` will be converted to strings
+        and joined together by `sep` via the joins function.
+
+        :param any data: An initial chunk of data
+        :param any more: Optional, any additional data to send
+        :param str sep: Optional, a separator to join the resulting output by
+        :param str end: Optional, a terminator appended to the resulting output
+
+        """
+        self._output_queue.append(joins(data, *more, sep=sep) + end)
+
+    def poll(self):
+        """Check status and process any queued IO."""
+        if self._socket.active:
+            if self.active:
+                self._check_idle()
+            # Process input through the command queue.
+            data = None
+            if self._socket.cmd_ready and self.active:
+                data = self._socket.get_command()
+                if data is not None:
+                    self._parse_input(data)
+            # Process output from the output queue.
+            output = None
+            if self._output_queue and self._socket:
+                # This can later be made more sophisticated with paged output,
+                # width-reformatting, etc., but for now it's pretty simple.
+                if data is None:
+                    # We didn't get a command this poll, so we need to send
+                    # them a newline before anything else.
+                    self._output_queue.appendleft("\n")
+                output = "".join(self._output_queue)
+                self._socket.send_cc(output)
+                self._output_queue.clear()
+            # Send them a prompt if there was any input or output.
+            if (data is not None or output is not None) and self.active:
+                self._socket.send_cc(self._get_prompt())
+        # All the IO is done, do a final state check.
+        if not self.active and "closed" not in self.flags:
+            with EVENTS.fire("session_ended", self):
+                # Hooks to this event cannot send any output to the client,
+                # this is its last poll.
+                self.flags.drop("close")
+                self.flags.add("closed")
+
+    def close(self, reason, log_msg=""):
+        """Close this session.
+
+        The session will not be closed immediately, but will be flagged for
+        closing at the end of its next poll.
+
+        :param str reason: The reason this session is being closed, it will
+                           be sent to the client prior to closing
+        :param str log_msg: Optional, a custom log message for the closing
+
+        """
+        if not log_msg:
+            log_msg = joins("Closing session", self)
+        log.info(log_msg)
+        self.send(reason)
+        self.flags.add("close")
+
+
+# We create a global SessionManager here for convenience, and while the server
+# will generally only need one to work with, they are NOT singletons and you
+# can make more SessionManager instances if you like.
+SESSIONS = SessionManager()
---
+++ tests/core/test_sessions.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+"""Tests for session management and client IO."""
+# Part of Atria MUD Server (https://github.com/whutch/atria)
+# :copyright: (c) 2008 - 2014 Will Hutcheson
+# :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
+
+from atria import settings
+from atria.core.sessions import SessionManager
+
+
+class TestSessionManager:
+
+    """A collection of tests for session management."""
+
+    sessions = None
+    session = None
+    prompt = ""
+
+    # noinspection PyDocstring
+    class _FakeSocket:
+
+        def __init__(self):
+            self.active = True
+            self._idle = 0
+            self._commands = []
+            self._output = []
+
+        @staticmethod
+        def addrport():
+            return "127.0.0.1:56789"
+
+        @property
+        def cmd_ready(self):
+            return bool(self._commands)
+
+        def get_command(self):
+            if self._commands:
+                return self._commands.pop(0)
+
+        def idle(self):
+            return self._idle
+
+        def send_cc(self, output):
+            return self._output.append(output)
+
+        # noinspection PyPep8Naming,PyDocstring
+        class sock:
+
+            @staticmethod
+            def close():
+                pass
+
+    socket = _FakeSocket()
+
+    def test_session_manager_create(self):
+        """Test that we can create a session manager.
+
+        This is currently redundant, importing the sessions package already
+        creates one, but we can keep the it for symmetry and in case
+        that isn't always so.
+
+        """
+        type(self).sessions = SessionManager()
+        assert self.sessions
+
+    def test_session_create(self):
+        """Test that we can create a session."""
+        type(self).session = self.sessions.create(self.socket)
+        assert self.session
+
+    def test_session_find_by_socket(self):
+        """Test that we can find a session by its socket."""
+        assert self.sessions.find_by_socket(self.socket) is self.session
+
+    def test_session_properties(self):
+        """Test that the properties of a session return something."""
+        assert self.session.active and self.session.address
+
+    def test_session_active(self):
+        """Test that we can determine if a session should be closed."""
+        assert self.session.active
+        self.session._socket = None
+        assert not self.session.active
+        self.session._socket = self.socket
+        self.session.flags.toggle("close")
+        assert not self.session.active
+        self.session.flags.toggle("close")
+        assert self.session.active
+
+    def test_session_get_prompt(self):
+        """Test that we can generate the prompt for a session."""
+        type(self).prompt = self.session._get_prompt()
+        assert self.prompt
+
+    def test_session_send(self):
+        """Test that we can send output to a session."""
+        self.session.send("This", "is a", "test.")
+        self.session.send("\n", "t", 10, 8, 2014, sep="\\", end="\r\n")
+        assert self.session._output_queue.popleft() == "This is a test.\n"
+        assert self.session._output_queue.popleft() == "\n\\t\\10\\8\\2014\r\n"
+        assert not self.session._output_queue
+
+    def test_session_parse_input(self):
+        """Test that a session can parse input."""
+        self.session._parse_input("test")
+        self.session._parse_input("beep beep")
+        assert self.session._output_queue.popleft() == "You sent: test\n"
+        assert self.session._output_queue.popleft() == "You sent: beep beep\n"
+        assert not self.session._output_queue
+
+    def test_session_poll(self):
+        """Test that we can poll a session to process its queued IO."""
+        self.socket._commands.append("test")
+        assert self.socket.cmd_ready
+        self.session.poll()
+        assert not self.socket.cmd_ready
+        assert self.socket._output.pop(0) == "You sent: test\n"
+        assert self.socket._output.pop(0) == self.prompt
+        assert not self.socket._output
+
+    def test_session_check_idle(self):
+        """Test that we can determine if a session is idle."""
+        assert not self.session.flags.has_any("idle", "close")
+        self.session._check_idle()
+        assert not self.session.flags.has_any("idle", "close")
+        # They've gone idle.
+        self.socket._idle = settings.IDLE_TIME
+        self.session._check_idle()
+        assert "idle" in self.session.flags
+        assert "close" not in self.session.flags
+        # They came back.
+        self.socket._idle = 0
+        self.session._check_idle()
+        assert not self.session.flags.has_any("idle", "close")
+        assert not self.session._output_queue
+        assert self.session.active
+        # Now they've been idle for a really long time.
+        self.socket._idle = settings.IDLE_TIME_MAX
+        self.session._check_idle()
+        assert self.session.flags.has("close")
+        assert (self.session._output_queue.popleft() ==
+                "^RDisconnecting due to inactivity. Goodbye!^~\n")
+        assert not self.session.active
+
+    def test_session_manager_poll(self):
+        """Test that we can poll a session manager to poll all its sessions."""
+        # Create a couple more sessions to test with.
+        sessions = [self.session,
+                    self.sessions.create(self._FakeSocket()),
+                    self.sessions.create(self._FakeSocket())]
+        # Change them around a bit and then poll them all.
+        sessions[0]._socket._commands.append("test")
+        sessions[1]._socket._commands.append("test")
+        self.sessions.poll()
+        # The first session was flagged for closing in the previous test,
+        # so its input should have been ignored and its flag should have
+        # been changed from close to closed.
+        assert sessions[0]._socket.cmd_ready
+        assert not sessions[0]._socket._output
+        assert not sessions[0].active
+        assert "closed" in sessions[0].flags
+        # The second session should have been parsed and output returned.
+        assert not sessions[1]._socket.cmd_ready
+        assert sessions[1]._socket._output.pop(0) == "You sent: test\n"
+        assert sessions[1]._socket._output.pop(0) == self.prompt
+        assert not sessions[1]._socket._output
+        assert sessions[1].active
+        # And the third session didn't do anything, so should be unchanged.
+        assert not sessions[2]._socket.cmd_ready
+        assert not sessions[2]._socket._output
+        assert sessions[2].active
+
+    def test_session_close(self):
+        """Test that we can close a session."""
+        assert len(self.sessions._sessions) == 3
+        session = self.sessions._sessions[2]
+        assert session.active
+        session.close("bye cause reasons.")
+        assert not session.active
+        assert session._output_queue.popleft() == "bye cause reasons.\n"
+        assert not session._output_queue
+
+    def test_session_manager_prune(self):
+        """Test that we can prune the dead sessions from a session manager."""
+        assert len(self.sessions._sessions) == 3
+        self.sessions.prune()
+        assert len(self.sessions._sessions) == 1
