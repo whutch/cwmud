b6f8066add1f85e115560789af7a1ac15f343ea1
Will Hutcheson <will.hutcheson@gmail.com>
2014-11-12 08:35:57 -0600

    Server State Saving
    Add save_state and load_state functions to server.py module. Update boot
    function to look for an existing state file and load it if found.
    
    The state system at this point is just a framework, individual modules
    should hook the server_save_state and server_load_state events to push in
    and pull out their data for processing.

--- atria/core/server.py
+++ atria/core/server.py
@@ -16,6 +16,7 @@ from .sessions import SESSIONS
 from .shells import STATES, SHELLS, Shell, WeakValueDictionary
 from .timing import TIMERS
 from .utils.funcs import joins
+from .opt.pickle import PickleStore
 
 
 log = get_logger("server")
@@ -82,6 +83,46 @@ def _hook_client_disconnected(client):
         session._socket = None
 
 
+def save_state(pass_to_pid=None):
+    """Dump a serialized server state to file.
+
+    This function just dumps a state dict into a pickle file, all the actual
+    data processing and sanitizing should be done by individual modules
+    post-hooking the server_save_state event; each module should add any data
+    they want saved to the shared state dict before it is serialized.
+
+    :param int pass_to_pid: Optional, the PID of a new MUD server process that
+                            the state is being passed on to
+    :returns: None
+
+    """
+    server_data = PickleStore("server")
+    if server_data.has("state"):
+        raise KeyError("a server state file already exists")
+    state = {}
+    with EVENTS.fire("server_save_state", state, pass_to_pid):
+        server_data.put("state", state)
+        server_data.commit()
+
+
+def load_state():
+    """Load a serialized server state from file.
+
+    This function just reads a pickle file into a state dict, all the actual
+    data processing and sanitizing should be done by individual modules
+    post-hooking the server_load_state event; each module should pull any data
+    they want loaded from the shared state dict after it is deserialized.
+
+    :returns: None
+
+    """
+    server_data = PickleStore("server")
+    if not server_data.has("state"):
+        raise KeyError("no server state file exists")
+    state = server_data.get("state")
+    EVENTS.fire("server_load_state", state).now()
+
+
 def boot():
     """Initialize and boot up the MUD server.
 
@@ -91,6 +132,7 @@ def boot():
     with EVENTS.fire("server_init", no_pre=True):
         log.info("%s %s", settings.MUD_NAME_FULL, __version__)
         log.info("Initializing server")
+        server_data = PickleStore("server")
 
     with EVENTS.fire("server_boot"):
         log.info("Booting server")
@@ -102,6 +144,10 @@ def boot():
         if exists(greeting_path):
             with open(greeting_path) as greeting_file:
                 SESSIONS.greeting = greeting_file.read()
+        if server_data.has("state"):
+            load_state()
+            server_data.delete("state")
+            server_data.commit()
 
 
 def loop():
