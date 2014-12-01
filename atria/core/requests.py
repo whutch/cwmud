4534cf7aa5417bc044ecf155371b6d21c78de55b
Will Hutcheson <will.hutcheson@gmail.com>
2014-11-30 19:41:56 -0600

    Request System
    Expand functionality of Request class in atria/core/requests.py module.
    
    This new system is much simpler compared to those I cooked up in previous
    iterations of the MUD; resolve simply returns True or False depending on
    whether the a request gets resolved or not by the given input. This way
    the handler in sessions doesn't need to worry about handling different
    exceptions based on the outcome as was used in my previous system.

--- atria/core/requests.py
+++ atria/core/requests.py
@@ -6,6 +6,8 @@
 
 from .logs import get_logger
 from .utils.exceptions import AlreadyExists
+from .utils.funcs import joins
+from .utils.mixins import HasFlagsMeta, HasFlags, HasWeaksMeta, HasWeaks
 
 
 log = get_logger("requests")
@@ -53,13 +55,156 @@ class RequestManager:
         return request
 
 
-class Request:
+class _RequestMeta(HasFlagsMeta, HasWeaksMeta):
+    # To avoid multiple metaclass errors
+    pass
 
-    """An input request."""
 
-    def __init__(self):
+class Request(HasFlags, HasWeaks, metaclass=_RequestMeta):
+
+    """A request for input from a client."""
+
+    initial_prompt = "? "
+    repeat_prompt = "? "
+    confirm_prompt_yn = "'{data}', is that correct? (Y/N) "
+    confirm_prompt_repeat = "Repeat to confirm: "
+
+    def __init__(self, session, callback, **options):
         """Create an input request from this template."""
-        pass
+        super().__init__()
+        self.session = session
+        self.callback = callback
+        self.options = options.copy()
+        self._confirm = None
+
+    @property
+    def session(self):
+        """Return the current session for this request."""
+        return self._get_weak("session")
+
+    @session.setter
+    def session(self, new_session):
+        """Set the current session for this request.
+
+        :param _Session new_session: The session tied to this request
+        :returns: None
+
+        """
+        self._set_weak("session", new_session)
+
+    class ValidationFailed(Exception):
+        """An exception signaling that client input failed to validate."""
+
+        def __init__(self, msg):
+            self.msg = msg
+
+    def _validate(self, data):
+        """Validate client input toward this request.
+
+        Override this to perform any custom validation on subclasses. This
+        method should either return the validated data or raise the
+        ValidationFailed exception with an error message that will be sent to
+        the client, any other exceptions will be re-raised. The data can be
+        changed before returning it to allow for normalization, etc.
+
+        """
+        return data
+
+    def get_prompt(self):
+        """Generate the current prompt for this request."""
+        if self._confirm:
+            confirm = self.options.get("confirm")
+            if confirm == "y/n":
+                prompt = (self.options.get("confirm_prompt_yn")
+                          or self.confirm_prompt_yn)
+            elif confirm == "repeat":
+                prompt = (self.options.get("confirm_prompt_repeat")
+                          or self.confirm_prompt_repeat)
+            else:
+                raise ValueError(joins("bad value for request option "
+                                       "'confirm':", confirm))
+            return prompt.format(data=self._confirm)
+        else:
+            if not self.flags.has("prompted"):
+                self.flags.add("prompted")
+                initial_prompt = self.options.get("initial_prompt")
+                return initial_prompt or self.initial_prompt
+            else:
+                repeat_prompt = self.options.get("repeat_prompt")
+                return repeat_prompt or self.repeat_prompt
+
+    def resolve(self, data):
+
+        """Attempt to resolve this request with input from a client.
+
+        :param str data: The data to try and resolve this request with
+        :returns bool: Whether this request successfully resolved or not
+
+        """
+
+        resolved = False
+        error = None
+        confirm = self.options.get("confirm")
+        errors = self.options.get("errors", {})
+
+        def _validate(_data):
+            validator = self.options.get("validator")
+            if validator:
+                _data = validator(_data)
+            return self._validate(_data)
+
+        if self._confirm:
+            # They've already sent valid input, we just need to confirm it.
+            if confirm == "y/n":
+                if not "yes".startswith(data.lower()):
+                    error = errors.get("no", "Alright, what then?")
+            elif confirm == "repeat":
+                try:
+                    # We have to pass the repeated input through validation
+                    #  so that any normalization that happened to the original
+                    #  input happens to the repeated input as well.
+                    data = _validate(data)
+                except Request.ValidationFailed:
+                    # We don't really care if repeated input won't validate,
+                    #  we just want to know if it isn't the same, and the
+                    #  original data can't be None, so..
+                    data = None
+                finally:
+                    if data != self._confirm:
+                        error = errors.get("mismatch", "Input mismatch.")
+            else:
+                raise ValueError(joins("bad value for request option "
+                                       "'confirm':", confirm))
+            if error is None:
+                # Confirmation passed, request resolved.
+                resolved = True
+                # Make sure that the original input gets sent to the callback.
+                data = self._confirm
+
+        else:
+            # We're not confirming anything yet, do initial validation and
+            #  then figure out if we need to confirm or not.
+            try:
+                data = _validate(data)
+                if confirm:
+                    # We need to confirm this input, so store it for now and
+                    #  leave the request unresolved. The confirmation prompt
+                    #  will be sent by get_prompt at the end of this poll.
+                    self._confirm = data
+                else:
+                    # We don't need to confirm this input, request resolved.
+                    resolved = True
+            except Request.ValidationFailed as exc:
+                self.session.send(exc.msg)
+
+        if resolved:
+            self.callback(self.session, data)
+        elif error is not None:
+            if self._confirm:
+                # Confirmation failed, so start over.
+                self._confirm = None
+            self.session.send(error)
+        return resolved
 
 
 # We create a global RequestManager here for convenience, and while the
