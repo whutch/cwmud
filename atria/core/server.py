19a5e5df1f83b5d707cffdec3c254fb0f2e2a287
Will Hutcheson <will.hutcheson@gmail.com>
2014-12-22 08:15:52 -0600

    Server Module Cleanup
    Reorganize the server.py module a bit.

--- atria/core/server.py
+++ atria/core/server.py
@@ -21,130 +21,7 @@ from .opt.pickle import PickleStore
 
 
 log = get_logger("server")
-
 _SERVER_DATA = PickleStore("server")
-
-
-@SHELLS.register
-class ChatShell(Shell):
-
-    """A basic command shell for chatting."""
-
-    _verbs = WeakValueDictionary()
-    state = STATES.playing
-
-
-@COMMANDS.register
-class QuitCommand(Command):
-
-    """A command for quitting the server."""
-
-    def _action(self):
-        from .accounts import AccountMenu
-        self.session.menu = AccountMenu
-        self.session.shell = None
-
-
-@COMMANDS.register
-class SayCommand(Command):
-
-    """A command for saying stuff on the server."""
-
-    no_parse = True
-
-    def _action(self):
-        message = self.args[0].strip()
-        self.session.send(joins("You say, '", message, "'.", sep=""))
-
-
-@COMMANDS.register
-class ReloadCommand(Command):
-
-    """A command to reload the game server, hopefully without interruption.
-
-    This is similar to the old ROM-style copyover, except that we try and
-    preserve a complete game state rather than just the open connections.
-
-    """
-
-    def _action(self):
-        self.session.send("Starting server reload, hold on to your butt.")
-        raise ServerReload
-
-
-ChatShell.add_verbs(QuitCommand, "quit")
-ChatShell.add_verbs(SayCommand, "say", "'")
-ChatShell.add_verbs(ReloadCommand, "reload")
-
-
-def _client_connected(client):
-    """Fire an event when a new client connects."""
-    with EVENTS.fire("client_connected", client, no_pre=True):
-        log.info("Incoming connection from %s", client.addrport())
-
-
-def _client_disconnected(client):
-    """Fire and event when a client disconnects for any reason."""
-    with EVENTS.fire("client_disconnected", client, no_pre=True):
-        log.info("Lost connection from %s", client.addrport())
-
-
-@EVENTS.hook("client_connected")
-def _hook_client_connected(client):
-    session = SESSIONS.create(client)
-    with EVENTS.fire("session_started", session):
-        session.send(SESSIONS.greeting)
-        from .accounts import AccountMenu
-        session.menu = AccountMenu
-
-
-@EVENTS.hook("client_disconnected")
-def _hook_client_disconnected(client):
-    session = SESSIONS.find_by_client(client)
-    if session:
-        session._socket = None
-
-
-def save_state(pass_to_pid=None):
-    """Dump a serialized server state to file.
-
-    This function just dumps a state dict into a pickle file, all the actual
-    data processing and sanitizing should be done by individual modules
-    post-hooking the server_save_state event; each module should add any data
-    they want saved to the shared state dict before it is serialized.
-
-    :param int pass_to_pid: Optional, the PID of a new MUD server process that
-                            the state is being passed on to
-    :returns: None
-
-    """
-    if _SERVER_DATA.has("state"):
-        raise KeyError("a server state file already exists")
-    log.info("Starting game state save")
-    state = {}
-    with EVENTS.fire("server_save_state", state, pass_to_pid):
-        _SERVER_DATA.put("state", state)
-        _SERVER_DATA.commit()
-    log.info("Game state save successful")
-
-
-def load_state():
-    """Load a serialized server state from file.
-
-    This function just reads a pickle file into a state dict, all the actual
-    data processing and sanitizing should be done by individual modules
-    post-hooking the server_load_state event; each module should pull any data
-    they want loaded from the shared state dict after it is deserialized.
-
-    :returns: None
-
-    """
-    if not _SERVER_DATA.has("state"):
-        raise KeyError("no server state file exists")
-    log.info("Starting game state load")
-    state = _SERVER_DATA.get("state")
-    EVENTS.fire("server_load_state", state).now()
-    log.info("Game state load successful")
 
 
 def boot():
@@ -156,6 +33,14 @@ def boot():
     with EVENTS.fire("server_init", no_pre=True):
         log.info("%s %s", settings.MUD_NAME_FULL, __version__)
         log.info("Initializing server")
+
+    def _client_connected(client):
+        with EVENTS.fire("client_connected", client, no_pre=True):
+            log.info("Incoming connection from %s", client.addrport())
+
+    def _client_disconnected(client):
+        with EVENTS.fire("client_disconnected", client, no_pre=True):
+            log.info("Lost connection from %s", client.addrport())
 
     with EVENTS.fire("server_boot"):
         log.info("Booting server")
@@ -207,3 +92,113 @@ def loop():
         if not reloading:
             with EVENTS.fire("server_shutdown", no_post=True):
                 log.info("Server shutdown complete")
+
+
+def save_state(pass_to_pid=None):
+    """Dump a serialized server state to file.
+
+    This function just dumps a state dict into a pickle file, all the actual
+    data processing and sanitizing should be done by individual modules
+    post-hooking the server_save_state event; each module should add any data
+    they want saved to the shared state dict before it is serialized.
+
+    :param int pass_to_pid: Optional, the PID of a new MUD server process that
+                            the state is being passed on to
+    :returns: None
+
+    """
+    if _SERVER_DATA.has("state"):
+        raise KeyError("a server state file already exists")
+    log.info("Starting game state save")
+    state = {}
+    with EVENTS.fire("server_save_state", state, pass_to_pid):
+        _SERVER_DATA.put("state", state)
+        _SERVER_DATA.commit()
+    log.info("Game state save successful")
+
+
+def load_state():
+    """Load a serialized server state from file.
+
+    This function just reads a pickle file into a state dict, all the actual
+    data processing and sanitizing should be done by individual modules
+    post-hooking the server_load_state event; each module should pull any data
+    they want loaded from the shared state dict after it is deserialized.
+
+    :returns: None
+
+    """
+    if not _SERVER_DATA.has("state"):
+        raise KeyError("no server state file exists")
+    log.info("Starting game state load")
+    state = _SERVER_DATA.get("state")
+    EVENTS.fire("server_load_state", state).now()
+    log.info("Game state load successful")
+
+
+@EVENTS.hook("client_connected")
+def _hook_client_connected(client):
+    session = SESSIONS.create(client)
+    with EVENTS.fire("session_started", session):
+        session.send(SESSIONS.greeting)
+        from .accounts import AccountMenu
+        session.menu = AccountMenu
+
+
+@EVENTS.hook("client_disconnected")
+def _hook_client_disconnected(client):
+    session = SESSIONS.find_by_client(client)
+    if session:
+        session._socket = None
+
+
+@SHELLS.register
+class ChatShell(Shell):
+
+    """A basic command shell for chatting."""
+
+    _verbs = WeakValueDictionary()
+    state = STATES.playing
+
+
+@COMMANDS.register
+class QuitCommand(Command):
+
+    """A command for quitting the server."""
+
+    def _action(self):
+        from .accounts import AccountMenu
+        self.session.menu = AccountMenu
+        self.session.shell = None
+
+
+@COMMANDS.register
+class ReloadCommand(Command):
+
+    """A command to reload the game server, hopefully without interruption.
+
+    This is similar to the old ROM-style copyover, except that we try and
+    preserve a complete game state rather than just the open connections.
+
+    """
+
+    def _action(self):
+        self.session.send("Starting server reload, hold on to your butt.")
+        raise ServerReload
+
+
+@COMMANDS.register
+class SayCommand(Command):
+
+    """A command for saying stuff on the server."""
+
+    no_parse = True
+
+    def _action(self):
+        message = self.args[0].strip()
+        self.session.send(joins("You say, '", message, "'.", sep=""))
+
+
+ChatShell.add_verbs(QuitCommand, "quit")
+ChatShell.add_verbs(ReloadCommand, "reload")
+ChatShell.add_verbs(SayCommand, "say", "'")
