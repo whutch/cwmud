a7a435968dfb79ddaa636f14e35e0d6f2fb221b6
Will Hutcheson <will.hutcheson@gmail.com>
2015-03-28 09:47:21 -0500

    Conftest Changes
    Add a log message and Redis channel message to signal the start of testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,28 @@
 
 from os.path import abspath, dirname, join
 import sys
+import time
+
+import redis
 
 # Until Atria is an installable package, we need to add it to our Python path
 #  so that we can do "from atria" imports in the tests.
 TEST_ROOT = dirname(abspath(__file__))
 sys.path.insert(0, dirname(TEST_ROOT))
 
-
 from atria import ROOT_DIR, settings
+
 
 # Change the log path during testing
 settings.LOG_PATH = join(ROOT_DIR, "logs", "test.log")
+
+
+from atria.core.logs import get_logger
+
+log = get_logger("tests")
+
+
+# Send out a message to signal the starts of a test run
+rdb = redis.StrictRedis(decode_responses=True)
+rdb.publish("tests-start", time.time())
+log.debug("====== TESTS START ======")
