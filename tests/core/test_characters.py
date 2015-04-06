a491e3b809f3cf2c6e8d79d4e6a19ab26198292b
Will Hutcheson <will.hutcheson@gmail.com>
2015-04-06 12:54:09 -0500

    Character Tests
    Add barebones tests module for characters.

---
+++ tests/core/test_characters.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Tests for character entities."""
+# Part of Atria MUD Server (https://github.com/whutch/atria)
+# :copyright: (c) 2008 - 2015 Will Hutcheson
+# :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
+
+from atria.core.characters import Character
+
+
+class TestCharacters:
+
+    """A collection of tests for character entities."""
+
+    character = Character()
