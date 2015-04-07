9f4f3c84912ec47146b0c346ca91d267c35bb51b
Will Hutcheson <will.hutcheson@gmail.com>
2015-04-07 11:48:37 -0500

    World Tests
    Add barebones tests module for world entities.

---
+++ tests/core/test_world.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Tests for world entities."""
+# Part of Atria MUD Server (https://github.com/whutch/atria)
+# :copyright: (c) 2008 - 2015 Will Hutcheson
+# :license: MIT (https://github.com/whutch/atria/blob/master/LICENSE.txt)
+
+from atria.core.world import Room
+
+
+class TestRooms:
+
+    """A collection of tests for room entities."""
+
+    room = Room()
