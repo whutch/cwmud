894de3c8c691370ed4458b81c6612789951508d1
Will Hutcheson <will.hutcheson@gmail.com>
2015-12-23 19:44:23 -0600

    Mixins Test Coverage
    Extend test coverage for mixins module.

--- tests/core/utils/test_mixins.py
+++ tests/core/utils/test_mixins.py
@@ -18,7 +18,10 @@ class TestHasFlags:
 
         def __init__(self):
             super().__init__()
-            self.some_attribute = 5
+            self.changed = 0
+
+        def _flags_changed(self):
+            self.changed += 1
 
     instance = _TestClass()
 
@@ -40,8 +43,24 @@ class TestHasFlags:
         self.instance.flags.add(1, 2)
         assert 1 in self.instance.flags and 2 in self.instance.flags
 
+    def test_changed_callback(self):
+        """Test that modifying the flag set triggers a callback."""
+        had_changed = self.instance.changed
+        self.instance.flags.toggle(3)
+        assert self.instance.changed == had_changed + 1
+        self.instance.flags.toggle(3)
+        assert self.instance.changed == had_changed + 2
+        # Ensure that nothing breaks if there is no owner.
+        owner_ref = self.instance.flags._owner_ref
+        self.instance.flags._owner_ref = None
+        self.instance.flags.toggle(3)
+        assert self.instance.changed == had_changed + 2
+        self.instance.flags._owner_ref = owner_ref
+        self.instance.flags.toggle(3)
+        assert self.instance.changed == had_changed + 3
+
     def test_flags_as_tuple(self):
-        """Test that the all property returns the current flags."""
+        """Test that we cam get the current flags as a tuple."""
         assert set(self.instance.flags.as_tuple) == {"test", 1, 2}
 
     def test_iter_flags(self):
@@ -56,9 +75,11 @@ class TestHasFlags:
 
     def test_drop_multiple_flags(self):
         """Test that we can drop multiple flags at once."""
-        assert 1 in self.instance.flags and 2 in self.instance.flags
-        self.instance.flags.drop(1, 2)
-        assert 1 not in self.instance.flags and 2 not in self.instance.flags
+        assert (1 in self.instance.flags and 2 in self.instance.flags
+                and 3 not in self.instance.flags)
+        self.instance.flags.drop(1, 2, 3)
+        assert (1 not in self.instance.flags and 2 not in self.instance.flags
+                and 3 not in self.instance.flags)
 
     def test_toggle_flags(self):
         """Test that we can toggle one flag in the flag set."""
@@ -85,6 +106,8 @@ class TestHasFlags:
         assert not self.instance.flags.has("test")
         self.instance.flags.toggle("test")
         assert self.instance.flags.has("test")
+        # Ensure that the class itself has a separate flag set.
+        assert not self._TestClass.flags
 
     def test_has_multiple_flags(self):
         """Test that we can check for multiple flags through the has method."""
@@ -96,6 +119,21 @@ class TestHasFlags:
         """Test that we can check for flags through the has_any method."""
         assert not self.instance.flags.has_any("nope", 3, 4)
         assert self.instance.flags.has_any("test", 3, 4)
+
+    def test_no_owner(self):
+        """Test adding, dropping, and toggling with no owner."""
+        owner_ref = self.instance.flags._owner_ref
+        self.instance.flags._owner_ref = None
+        assert not self.instance.flags.has(5)
+        self.instance.flags.add(5)
+        assert self.instance.flags.has(5)
+        self.instance.flags.drop(5)
+        assert not self.instance.flags.has(5)
+        self.instance.flags.toggle(5)
+        assert self.instance.flags.has(5)
+        self.instance.flags.toggle(5)
+        assert not self.instance.flags.has(5)
+        self.instance.flags._owner_ref = owner_ref
 
 
 class TestHasTags:
@@ -196,6 +234,42 @@ class TestHasWeaks:
             """
             self._set_weak("weak_ref", obj)
 
+    def test_class_methods(self):
+        """Test that we can get, set and delete weaks on classes."""
+        instance = self._TestClass()
+        assert not self._TestClass._get_weak("test")
+        self._TestClass._set_weak("test", instance)
+        assert self._TestClass._get_weak("test") is instance
+        # Ensure that the weak reference was only added to the class and
+        # not its instances.
+        assert not instance._get_weak("test")
+        self._TestClass._del_weak("test")
+        assert not self._TestClass._get_weak("test")
+        # Make sure deleting a non-existent weak reference doesn't explode.
+        self._TestClass._del_weak("test")
+        # Test that setting a weak to None removes it entirely.
+        self._TestClass._set_weak("test", instance)
+        assert self._TestClass._get_weak("test")
+        self._TestClass._set_weak("test", None)
+        # noinspection PyUnresolvedReferences
+        assert "test" not in self._TestClass._weak_refs
+        del instance
+        gc.collect()
+
+    def test_instance_methods(self):
+        """Test that we can get, set and delete weaks on instances."""
+        instance = self._TestClass()
+        assert not instance._get_weak("test")
+        instance._set_weak("test", instance)
+        assert instance._get_weak("test") is instance
+        # Ensure that the weak reference was only added to the instance and
+        # not its class.
+        assert not self._TestClass._get_weak("test")
+        instance._del_weak("test")
+        assert not instance._get_weak("test")
+        del instance
+        gc.collect()
+
     def test_weak_ref_one_way(self):
         """Test linking an object to another through a weak property."""
         assert self._TestClass.count == 0
@@ -251,16 +325,21 @@ class TestHasParent:
     class _C(_B):
         pass
 
-    class _D(_B):
+    class _D:
         pass
 
-    class _E(_C, _D):
+    class _E(_D, _C):
+        pass
+
+    class _F(_E, _B):
         pass
 
     def test_get_lineage(self):
         """Test that we can get an objects lineage through the parents."""
         assert (tuple(self._C.get_lineage()) ==
                 (self._C, self._B, self._A))
+        assert (tuple(self._E.get_lineage()) ==
+                (self._E, self._C, self._B, self._A))
 
     def test_get_lineage_higher_priority(self):
         """Test that we can get the higher priority objects in a lineage."""
@@ -279,12 +358,17 @@ class TestHasParent:
         assert (tuple(self._C.get_lineage(priority=1)) ==
                 (self._B, self._A))
         assert (tuple(self._C.get_lineage(priority=-1)) == ())
+        self._E.parent_first = True
+        assert (tuple(self._E.get_lineage()) ==
+                (self._B, self._A, self._C, self._E))
 
     def test_get_ancestors(self):
         """Test that we can get the ancestors of an object."""
         assert (tuple(self._C.get_ancestors()) ==
                 (self._B, self._A))
         assert tuple(self._A.get_ancestors()) == ()
+        assert (tuple(self._F.get_ancestors()) ==
+                (self._E, self._C, self._B, self._A))
 
     def test_has_ancestor(self):
         """Test that we can see if an object has an ancestor."""
