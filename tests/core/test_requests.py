97eb3646d78817941a5e803f7731ed41cf4d2267
Will Hutcheson <will.hutcheson@gmail.com>
2016-01-03 14:13:02 -0600

    Requests Test Coverage
    Extend test coverage for requests module.

--- tests/core/test_requests.py
+++ tests/core/test_requests.py
@@ -48,6 +48,11 @@ class TestRequests:
         class TestRequest(Request):
             """A test request."""
 
+            def _validate(self, data):
+                if not isinstance(data, str):
+                    raise self.ValidationFailed("should be a str")
+                return data.upper()
+
         type(self).request_class = TestRequest
         assert "TestRequest" in self.requests._requests
 
@@ -77,3 +82,73 @@ class TestRequests:
         # noinspection PyCallingNonCallable
         type(self).request = self.request_class(self.session, None)
         assert self.request
+
+    def test_request_validation(self):
+        """Test that we can validate input for a request."""
+        assert Request._validate(self.request, "toot") == "toot"
+        assert self.request._validate("toot") == "TOOT"
+        with pytest.raises(Request.ValidationFailed):
+            self.request._validate(123)
+
+    def test_request_get_prompt(self):
+        """Test that we can generate request prompts."""
+        # First test without previous input to confirm.
+        assert self.request.get_prompt() == self.request.repeat_prompt
+        assert self.request.get_prompt() == self.request.repeat_prompt
+        self.request.initial_prompt = "Yeah? "
+        self.request.flags.drop("prompted")
+        assert self.request.get_prompt() == self.request.initial_prompt
+        assert self.request.get_prompt() == self.request.repeat_prompt
+        # Then test confirmation prompts.
+        self.request._confirm = "yeah"
+        self.request.confirm = Request.CONFIRM_YES
+        assert (self.request.get_prompt() ==
+                self.request.confirm_prompt_yn.format(data="yeah"))
+        self.request.confirm = Request.CONFIRM_REPEAT
+        assert (self.request.get_prompt() ==
+                self.request.confirm_prompt_repeat)
+        self.request.confirm = "bad confirm type"
+        with pytest.raises(ValueError):
+            self.request.get_prompt()
+        self.request.flags.drop("prompted")
+
+    def test_request_resolve_no_confirm(self):
+        """Test that we can resolve a request without confirmation."""
+        self.request._confirm = None
+        self.request.confirm = None
+        assert self.request.resolve("test") is True
+        assert not self.request._confirm
+        self.request.options["validator"] = lambda value: value
+        self.request.callback = lambda session, data: session.send(data)
+        assert self.request.resolve("another test") is True
+        assert self.request.session._output.pop() == "ANOTHER TEST\n"
+
+    def test_request_resolve_yn_confirm(self):
+        """Test that we can resolve a request with yes/no confirmation."""
+        self.request.confirm = Request.CONFIRM_YES
+        assert self.request.resolve("test") is False
+        assert self.request._confirm == "TEST"
+        assert self.request.resolve("no") is False
+        assert not self.request._confirm
+        assert self.request.resolve("test") is False
+        assert self.request._confirm == "TEST"
+        assert self.request.resolve("yes") is True
+
+    def test_request_resolve_repeat_confirm(self):
+        """Test that we can resolve a request with repeat confirmation."""
+        self.request._confirm = None
+        self.request.confirm = Request.CONFIRM_REPEAT
+        assert self.request.resolve(123) is False
+        assert not self.request._confirm
+        assert self.request.resolve("test") is False
+        assert self.request.resolve("testtt") is False
+        assert self.request.resolve("test") is False
+        assert self.request.resolve(123) is False
+        assert self.request.resolve("test") is False
+        assert self.request.resolve("test") is True
+
+    def test_request_resolve_invalid_confirm(self):
+        """Test that we can detect a bad request confirmation type."""
+        self.request.confirm = "bad confirm type"
+        with pytest.raises(ValueError):
+            self.request.resolve("test")
