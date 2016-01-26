1581ba294c1133bae561f8aef8531fcd2b723c39
Will Hutcheson <will.hutcheson@gmail.com>
2016-01-26 07:55:26 -0600

    Channel Manager
    Add ChannelManager class to transition to registered channels.

--- atria/core/channels.py
+++ atria/core/channels.py
@@ -7,10 +7,55 @@
 from weakref import WeakSet
 
 from .logs import get_logger
+from .utils.exceptions import AlreadyExists
 from .utils.funcs import joins
 
 
 log = get_logger("channels")
+
+
+class ChannelManager:
+
+    """A manager for channels.
+
+    This is a convenience manager and is not required for the server to
+    function.  All of its functionality can be achieved by subclassing,
+    instantiating, and referencing channels directly.
+
+    """
+
+    def __init__(self):
+        """Create a new channel manager."""
+        self._channels = {}
+
+    def __contains__(self, channel):
+        return channel in self._channels
+
+    def __getitem__(self, channel):
+        return self._channels[channel]
+
+    def register(self, name, channel):
+        """Register a channel.
+
+        :param str name: The name to register the channel under
+        :param Channel channel: The channel to register
+        :returns Channel: The registered channel
+        :raises AlreadyExists: If a channel with `name` already exists
+        :raises TypeError: If `channel` is not an instance of Channel
+
+        """
+        if not isinstance(channel, Channel):
+            raise TypeError("must be an instance Channel to register")
+        if name in self._channels:
+            raise AlreadyExists(name, self._channels[name], channel)
+        self._channels[name] = channel
+        return channel
+
+
+# We create a global ChannelManager here for convenience, and while the
+# server will generally only need one to work with, they are NOT singletons
+# and you can make more ChannelManager instances if you like.
+CHANNELS = ChannelManager()
 
 
 class Channel:
